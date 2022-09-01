1d8299a0705a1903386b78e5a33a41d3565465e7
João Pedro Granelli <joao.granelli123@gmail.com>
2022-09-01 15:02:47 -0300

    Add files via upload

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import { ActiveLink } from '../ActiveLink'
+import { SignInButton } from '../SignInButton'
+
+import styles from './styles.module.scss'
+
+export function Header () {
+    
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="Logo Ig News" />
+                <nav>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                       <a>Home</a>
+                    </ActiveLink>
+
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    )
+}
---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,22 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+import styles from './styles.module.scss';
+
+export function SignInButton () {
+    const { data: session } = useSession()
+
+    return session ? (
+        <button className={styles.signInButton} type="button" onClick={() => signOut()}>
+        <FaGithub color="#84d361"/>
+        {session.user.name}
+        <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+) : (
+        <button className={styles.signInButton} type="button" onClick={() => signIn('github')}>
+        <FaGithub color="#eba417"/>
+        Sign In with GitHub
+    </button>
+); 
+}
